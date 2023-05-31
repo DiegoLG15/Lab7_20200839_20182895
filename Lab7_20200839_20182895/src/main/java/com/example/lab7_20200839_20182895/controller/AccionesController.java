4a6bd73e660519e0f46a0c03c07e8150d7110e8e
DiegoLG15 <a20200839@pucp.edu.pe>
2023-05-31 02:21:08 -0500

    avance pregunta4

--- Lab7_20200839_20182895/src/main/java/com/example/lab7_20200839_20182895/controller/AccionesController.java
+++ Lab7_20200839_20182895/src/main/java/com/example/lab7_20200839_20182895/controller/AccionesController.java
@@ -1,5 +1,7 @@
 package com.example.lab7_20200839_20182895.controller;
 import com.example.lab7_20200839_20182895.entity.Acciones;
+import com.example.lab7_20200839_20182895.entity.Usuario;
+import com.example.lab7_20200839_20182895.repository.AccionesRepository;
 import com.example.lab7_20200839_20182895.repository.PagosRespository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,4 +13,23 @@ import java.util.List;
 @RestController
 @RequestMapping("/acciones")
 public class AccionesController {
+
+    private final AccionesRepository accionesRepository;
+
+    public AccionesController(AccionesRepository accionesRepository) {
+        this.accionesRepository = accionesRepository;
+    }
+
+    @PostMapping(value = "/guardar")
+    public ResponseEntity<HashMap<String,Object>> guardarAcciones(
+            @RequestBody Acciones acciones,
+            @RequestParam (value = "fetchId",required = false) boolean fetchId){
+        HashMap<String,Object> responseMap = new HashMap<>();
+        accionesRepository.save(acciones);
+        if (fetchId){
+            responseMap.put("id creado", acciones.getId());
+        }
+        responseMap.put("estado","creado");
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseMap);
+    }
 }
