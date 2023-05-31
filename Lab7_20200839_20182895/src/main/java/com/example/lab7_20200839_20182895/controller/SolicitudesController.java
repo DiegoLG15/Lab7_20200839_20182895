12516a61389a066b3932f67d1a30848473ad9dfa
william <william24jes@hotmail.com>
2023-05-31 12:37:37 -0500

    guardar solicitud listo

--- Lab7_20200839_20182895/src/main/java/com/example/lab7_20200839_20182895/controller/SolicitudesController.java
+++ Lab7_20200839_20182895/src/main/java/com/example/lab7_20200839_20182895/controller/SolicitudesController.java
@@ -6,9 +6,28 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
-import java.util.List;
 
 @RestController
 @RequestMapping("/solicitudes")
 public class SolicitudesController {
+    private final SolicitudesRepository solicitudesRepository;
+
+    public SolicitudesController(SolicitudesRepository solicitudesRepository) {
+        this.solicitudesRepository = solicitudesRepository;
+    }
+
+    @PostMapping(value = "/registro")
+    public ResponseEntity<HashMap<String,Object>> guardarSolicitud(
+            @RequestBody Solicitudes solicitudes,
+            @RequestParam (value = "fetchId",required = false) boolean fetchId){
+        HashMap<String,Object> responseMap = new HashMap<>();
+        solicitudes.setSolicitudEstado("pendiente");
+        solicitudesRepository.save(solicitudes);
+        if (fetchId){
+            responseMap.put("id creado", solicitudes.getId());
+        }
+        responseMap.put("Monto solicitado",solicitudes.getSolicitudMonto());
+        responseMap.put("id",solicitudes.getId());
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseMap);
+    }
 }
