0f33144f34d5cf6b36feecc920125d0599fd98b6
DiegoLG15 <a20200839@pucp.edu.pe>
2023-05-31 18:44:31 -0500

    p4 completa

--- Lab7_20200839_20182895/src/main/java/com/example/lab7_20200839_20182895/controller/AccionesController.java
+++ Lab7_20200839_20182895/src/main/java/com/example/lab7_20200839_20182895/controller/AccionesController.java
@@ -1,10 +1,10 @@
 package com.example.lab7_20200839_20182895.controller;
 import com.example.lab7_20200839_20182895.entity.Acciones;
-import com.example.lab7_20200839_20182895.entity.Usuario;
 import com.example.lab7_20200839_20182895.repository.AccionesRepository;
-import com.example.lab7_20200839_20182895.repository.PagosRespository;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -20,16 +20,24 @@ public class AccionesController {
         this.accionesRepository = accionesRepository;
     }
 
-    @PostMapping(value = "/guardar")
+    @PostMapping(value = "/save")
     public ResponseEntity<HashMap<String,Object>> guardarAcciones(
-            @RequestBody Acciones acciones,
-            @RequestParam (value = "fetchId",required = false) boolean fetchId){
+            @RequestBody Acciones acciones){
         HashMap<String,Object> responseMap = new HashMap<>();
         accionesRepository.save(acciones);
-        if (fetchId){
-            responseMap.put("id creado", acciones.getId());
-        }
-        responseMap.put("estado","creado");
+        responseMap.put("idCreado", acciones.getId());
         return ResponseEntity.status(HttpStatus.CREATED).body(responseMap);
+    }
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<HashMap<String,String>> gestionExcepcion(HttpServletRequest request) {
+
+        HashMap<String, String> responseMap = new HashMap<>();
+
+        if (request.getMethod().equals("POST")) {
+            responseMap.put("estado", "“error");
+            responseMap.put("msg", "Debe guardar una accion");
+
+        }
+        return ResponseEntity.badRequest().body(responseMap);
     }
 }
